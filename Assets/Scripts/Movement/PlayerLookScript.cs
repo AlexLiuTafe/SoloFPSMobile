f965a43a9ecdb5ac2addbcbebd1f43f2e9d09e61
AlexLiuTafe <alex_march1988@yahoo.com.au>
2019-08-20 11:15:40 +1000

    Initial commit

---
+++ Assets/Scripts/Movement/PlayerLookScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLookScript : MonoBehaviour
+{
+	[Header("Reference")]
+	public MasterController _moveJoystick;
+	public RotationalAxis _axis = RotationalAxis.lookX;
+	[Header("Sensitivity")]
+	[Range(0, 5)]
+	public float _lookSensitivityX = 10f;
+	public float _lookSensitivityY = 10f;
+	[Header("Y Rotaion Clamp")]
+	public float _maxY = 60f;
+	public float _minY = -60f;
+
+	private float _rotationY = 0;
+	// Start is called before the first frame update
+	void Start()
+	{
+		if(GetComponent<Rigidbody>())
+		{
+			GetComponent<Rigidbody>().freezeRotation = true;
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// For lookX and lookY (Diagonal Input)
+		if(_axis == RotationalAxis.lookXandY)
+		{
+			float _rotationX = transform.localEulerAngles.y + _moveJoystick.Horizontal * _lookSensitivityX;
+			_rotationY += _moveJoystick.Vertical * _lookSensitivityY;
+			_rotationY = Mathf.Clamp(_rotationY, _minY, _maxY);
+			// -rotationY because it is inverted
+			//(we put in X axis because it is dealing with rotation and we want to rotate in X to look up and down)
+			transform.localEulerAngles = new Vector3(-_rotationY, _rotationX, 0);
+
+		}
+		//For looking hHrizontal (rotate Y axis base on horizontal input)
+		else if (_axis == RotationalAxis.lookX)
+		{
+			transform.Rotate(0, _moveJoystick.Horizontal * _lookSensitivityX,0);
+		}
+		else //For looking Vertical (rotate X axis base on vertical input)
+		{
+			_rotationY += _moveJoystick.Vertical * _lookSensitivityY;
+			_rotationY = Mathf.Clamp(_rotationY, _minY, _maxY);
+			transform.localEulerAngles = new Vector3(-_rotationY, 0, 0);
+		}
+		
+	}
+
+}
+public enum RotationalAxis
+{
+	lookXandY, lookX, lookY,
+}
---
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+	[Header("Reference")]
+	public MasterController _moveJoystick;
+	private CharacterController _charC;
+	private Rigidbody _rigid;
+	[Header("Attributes")]
+	private float _currentSpeed;
+    public float _movementSpeed = 5f;
+	public float _dashSpeed = 10f;
+	public float _dashTime = 2f;
+	private Vector3 _motion;
+    
+
+	
+	private void Start()
+    {
+		_currentSpeed = _movementSpeed;
+		_charC = GetComponent<CharacterController>();
+		_rigid = GetComponent<Rigidbody>();
+    }
+    private void Update()
+	{
+		Move(_currentSpeed);
+		_charC.Move(_motion *Time.deltaTime);
+    }
+
+    private void Move(float speed)
+    {
+		Vector3 direction = new Vector3(_moveJoystick.Horizontal, 0, _moveJoystick.Vertical);
+		_motion.x = direction.x * speed;
+		_motion.z = direction.z * speed;
+		if(direction.magnitude >1f)
+		{
+			direction.Normalize();
+		}
+		
+	}
+	private void Dash()
+	{
+		StartCoroutine(DashIE(_dashSpeed, _movementSpeed, _dashTime));
+	}
+	#region IENumerator
+	IEnumerator DashIE(float startDash,float endDash, float delay)
+	{
+		_currentSpeed = startDash;
+		yield return new WaitForSeconds(delay);
+		_currentSpeed = endDash;
+	}
+	#endregion
+}
