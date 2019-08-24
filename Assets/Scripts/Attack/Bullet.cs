91ad1c0a10fc741fc9b156761c09861413c68b8a
AlexLiuTafe <alex_march1988@yahoo.com.au>
2019-08-24 11:41:41 +1000

    Shooting

---
+++ Assets/Scripts/Attack/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	public int damage;
+	private void OnTriggerEnter(Collider col)
+	{
+		Enemy enemy = col.GetComponent<Enemy>();
+	
+		if(enemy)
+		{
+			Destroy(gameObject);
+			//enemy.TakeDamage(damage);
+		}
+		
+			Destroy(gameObject);
+		
+	}
+}
---
+++ Assets/Scripts/Attack/Shoot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+	[Header("Attributes")]
+	private float _shootTimer;
+	public float shootRate = 1f;
+	public float bulletSpeed = 20f;
+	public GameObject bulletPrefab;
+	public Transform bulletClone;	
+	private void Start()
+	{
+		//GameObject cam = GameObject.FindGameObjectWithTag("MainCamera");
+		
+
+	}
+	public void Attack()
+	{
+		if(Time.time > _shootTimer)
+		{
+			//																						  Make bullet always facing camera direction
+			GameObject bulletGO = GameObject.Instantiate(bulletPrefab, Camera.main.transform.position, Camera.main.transform.rotation);
+			Bullet bullet = bulletGO.GetComponent<Bullet>();
+			bullet.transform.position = transform.position + Camera.main.transform.forward * 2; //Make the bullet spawn abit further from player so doesnt collide with player
+			Rigidbody rigid = bullet.GetComponent<Rigidbody>();
+			rigid.velocity = Camera.main.transform.forward * bulletSpeed;
+			bullet.transform.SetParent(bulletClone);
+			Destroy(bullet, 0.5f);
+			_shootTimer = Time.time + shootRate;
+		}
+		
+
+	}
+	
+}
